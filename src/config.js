fad9b6ecb683d4228782f9aecdfbc99ace15cea9
Angel <angelcilfone@hotmail.com>
2024-06-29 16:37:41 -0300

    Dependencias actualizadas

--- src/config.js
+++ src/config.js
@@ -2,4 +2,5 @@
 //export const BASE_URL = 'http://192.168.1.2:8000/api'
 
 //Production
-export const BASE_URL = 'https://cobroconnect.online/api'
+//export const BASE_URL = 'https://cobroconnect.online/api'
+export const BASE_URL = 'https://cobroconnect.com/api'
